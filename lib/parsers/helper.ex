6615dc027c4e9fd470c1cb1581d6fc8ffb9b296a
delameko <delameko@delameko-stone.com>
2017-10-04 18:13:20 +0100

    Remove unneeded String.trim in to_date_time

--- lib/parsers/helper.ex
+++ lib/parsers/helper.ex
@@ -20,8 +20,6 @@ defmodule ElixirFeedParser.Parsers.Helper do
   def to_date_time(date_time_string), do: to_date_time(date_time_string, "RFC_1123")
   def to_date_time(nil, _), do: nil
   def to_date_time(date_time_string, format) do
-    date_time_string = String.trim(date_time_string)
-
     case format do
       "ISO_8601" ->
         {:ok, date_time, _} = DateTime.from_iso8601(date_time_string)
